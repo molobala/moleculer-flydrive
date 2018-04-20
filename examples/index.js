2f48ea19755110f02948cd233f9b69f7b1d34218
Doumbia Mahamadou <doumbiamahamadou.ensate@gmail.com>
2018-04-20 14:11:35 +0100

    Format example code with two space indentations

--- examples/index.js
+++ examples/index.js
@@ -4,52 +4,52 @@ const FlyDrive = require("../");
 const { ServiceBroker } = require("moleculer");
 const broker = new ServiceBroker();
 broker.createService(FlyDrive(),{
-	actions: {
-		exists:{
-			async handler(ctx){
-				//let result = await this.storage.disk().exists(ctx.params.filename);
-				//let result = await this.storage.disk("local").exists(ctx.params.filename);
-				return await this.storage.exists(ctx.params.filename);//will use the default storage defined
-			}
-		},
-		delete: {
-			async handler(ctx){
-				return await this.storage.delete(ctx.params.filename);
-			}
-		},
-		getContent: {
-			async handler(ctx){
-				return await this.storage.get(ctx.params.filename,ctx.params.encoding)
-			}
-		},
-		create: {
-			async handler(ctx){
-				return await this.storage.put(ctx.params.filename,ctx.params.fileContent);
-			}
-		}
-	}
+  actions: {
+    exists:{
+      async handler(ctx){
+        //let result = await this.storage.disk().exists(ctx.params.filename);
+        //let result = await this.storage.disk("local").exists(ctx.params.filename);
+        return await this.storage.exists(ctx.params.filename);//will use the default storage defined
+      }
+    },
+    delete: {
+      async handler(ctx){
+        return await this.storage.delete(ctx.params.filename);
+      }
+    },
+    getContent: {
+      async handler(ctx){
+        return await this.storage.get(ctx.params.filename,ctx.params.encoding)
+      }
+    },
+    create: {
+      async handler(ctx){
+        return await this.storage.put(ctx.params.filename,ctx.params.fileContent);
+      }
+    }
+  }
 });
 
 // Start server
 broker.start().then(async () => {
 
-	// Call action
-	let exist = await broker.call("flydrive.exists", {
-		filename:"some-file.txt",
-		fileContent: "A content"
-	});
-	console.log(exist);
-	if(!exist){
-		console.log(await broker.call("flydrive.create", {
-			filename:"some-file.txt",
-			fileContent: "Content"
-		}))
-	}
-	console.log(await broker.call("flydrive.getContent", {
-		filename: "some-file.txt",
-		encoding: "utf-8"
-	}));
-	console.log(await broker.call("flydrive.delete", {
-		filename: "some-file.txt"
-	}));
+  // Call action
+  let exist = await broker.call("flydrive.exists", {
+    filename:"some-file.txt",
+    fileContent: "A content"
+  });
+  console.log(exist);
+  if(!exist){
+    console.log(await broker.call("flydrive.create", {
+      filename:"some-file.txt",
+      fileContent: "Content"
+    }))
+  }
+  console.log(await broker.call("flydrive.getContent", {
+    filename: "some-file.txt",
+    encoding: "utf-8"
+  }));
+  console.log(await broker.call("flydrive.delete", {
+    filename: "some-file.txt"
+  }));
 });
